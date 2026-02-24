5ece07e42e37848f170c2196780a22b25be47180
blue-berry-s <95601084+blue-berry-s@users.noreply.github.com>
2026-02-23 23:34:07 -0500

    added component folder

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+function Header(){
+    return(
+        <>
+            <header id="nav-bar">
+                <div id="left-icons">
+                    <img src="./assets/logos/TMU.jpg" alt="TMU Logo" />
+                    <h1>TMU EVENTS</h1>
+                </div>
+
+                <nav id="right-icons">
+                    <img src="./assets/icons/home.svg" alt="Home Button" onClick={() => goTo('/')} />
+                    <img src="./assets/icons/profile.svg" alt="Profile Button" />
+                </nav>
+            </header>
+        </>
+    );
+
+}
