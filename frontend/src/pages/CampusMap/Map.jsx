f41281c0db835bbf9a6692f8355b4a0e67c73bdb
donngai <donovanngai237@gmail.com>
2026-03-01 21:17:34 -0500

    Fixed coordinates for map pins

--- frontend/src/pages/CampusMap/Map.jsx
+++ frontend/src/pages/CampusMap/Map.jsx
@@ -3,15 +3,15 @@ import TMUMap from '../../assets/TMUMap.svg';
 import './Map.css';
 
 const locations = [
-  { id: "MAC", x: 59.9, y: 5.8 },
+  { id: "MAC", x: 59.9, y: 6.8 },
   { id: "KHB", x: 56.8, y: 44 },
   { id: 'JOR', x: 45.75, y: 43.8 },
   { id: 'POD', x: 45.75, y: 53.2 },
   { id: 'LIB', x: 45.75, y: 63.3 },
   { id: 'SLC', x: 40.62, y: 61.2 },
   { id: 'RAC', x: 56.82, y: 56.94 },
-  { id: 'EPH', x: 71.75, y: 44.27 },
-  { id: 'RCC', x: 70.37, y: 64.79 },
+  { id: 'EPH', x: 71.75, y: 45.27 },
+  { id: 'RCC', x: 73.37, y: 63 },
   { id: 'ENG', x: 67.4, y: 74.59 },
   { id: 'DCC', x: 62.16, y: 84.88 },
   { id: 'VIC', x: 50.7, y: 85.70 },
